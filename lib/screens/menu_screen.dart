75a4b21caedec0fdfb5b35d5449aa4ed63821127
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-14 21:54:10 +0530

    Created menu screen

---
+++ lib/screens/menu_screen.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_app/screens/add_user_screen.dart';
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  void onTapAddUserBtn(BuildContext context) {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (BuildContext context) => const AddUserScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Menu"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                onTapAddUserBtn(context);
+              },
+              child: const Text("Add User"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("View Users"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
