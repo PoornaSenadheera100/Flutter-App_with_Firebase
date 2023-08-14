6df141b22ef0276aabfd80c37cbd0b2a95f381a8
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-14 23:17:54 +0530

    Created addUser method

---
+++ lib/models/user.dart
@@ -0,0 +1,8 @@
+class User{
+  String name;
+  String email;
+  int age;
+  String phone;
+
+  User(this.name, this.email, this.age, this.phone);
+}
---
+++ lib/services/crud_service.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_app/models/response.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference _collection = _firestore.collection("User");
+
+class FirebaseCrud{
+  static Future<Response> addUser(String name, String email, int age, String phone) async{
+    Response response = Response();
+    DocumentReference documentReference = _collection.doc();
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "name": name,
+      "email":email,
+      "age":age,
+      "phone":phone
+    };
+
+    var result = await documentReference.set(data).whenComplete((){
+      response.code = 200;
+      response.message = "User added successfully!";
+    }).catchError(  (e){
+      response.code = 500;
+      response.message = e;
+    });
+
+    return response;
+  }
+}
